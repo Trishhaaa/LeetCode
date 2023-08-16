65c8a5ebae63d65a7cdd045d9a2fab0cdd78ecca
Trishhaaa <97701078+Trishhaaa@users.noreply.github.com>
2023-08-16 07:34:37 +0530

    Time: 0 ms (100%), Space: 40.2 MB (97.24%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -13,7 +13,6 @@ class Solution {
         ListNode slow=head;
         ListNode fast=head;
 
-        //putting the initial difference between slow and fast equal to n
         for(int i=0;i<n;i++){
             fast=fast.next;
         }
@@ -22,15 +21,13 @@ class Solution {
             return head.next;
         }
 
-        //when fast reaches the end, slow reaches the node before the node to be deleted.
         while(fast.next!=null){
             fast=fast.next;
             slow=slow.next;
         }
 
-        // now skip the node that has to be deleted
         slow.next=slow.next.next;
-        return head;
 
+        return head;
     }
 }
