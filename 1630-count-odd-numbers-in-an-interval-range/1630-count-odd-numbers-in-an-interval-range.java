babee45b9f7b614e2828e4c881486085684ec33b
Trishhaaa <97701078+Trishhaaa@users.noreply.github.com>
2023-08-31 01:38:07 +0530

    Time: 0 ms (100%), Space: 39 MB (76.32%) - LeetHub

---
+++ 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        if(low%2==0){
+            low++;
+        }
+
+        if(low>high){
+            return 0;
+        }
+        return (high-low)/2+1;
+        
+    }
+}
