a24f6586732706afd98a795a84aa7718295a50aa
Trishhaaa <97701078+Trishhaaa@users.noreply.github.com>
2023-08-30 22:02:03 +0530

    Time: 0 ms (100%), Space: 40.6 MB (46.41%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(needle.length()>haystack.length()){
+        if(haystack.length()<needle.length()){
             return -1;
         }
         if(needle.length()==0){
@@ -8,10 +8,11 @@ class Solution {
         }
 
         for(int i=0;i<=haystack.length()-needle.length();i++){
-            if((haystack.substring(i,i+needle.length())).equals(needle)){
+            if(haystack.substring(i,i+needle.length()).equals(needle)){
                 return i;
             }
         }
         return -1;
+        
     }
 }
