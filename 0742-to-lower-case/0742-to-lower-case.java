596a04503b4f91089c1a7d2822dd0a120d22471a
Trishhaaa <97701078+Trishhaaa@users.noreply.github.com>
2023-08-31 01:21:15 +0530

    Time: 1 ms (18.53%), Space: 40.7 MB (34.3%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,5 +1,14 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>=65 && s.charAt(i)<=90){
+                sb.append((char)(s.charAt(i)+32));
+            }
+            else{
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 }
