0e20f0eb30af79428abc045eca3c8aa768386333
Trishhaaa <97701078+Trishhaaa@users.noreply.github.com>
2023-09-02 22:07:06 +0530

    Time: 0 ms (100%), Space: 43 MB (64.96%) - LeetHub

--- 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
@@ -1,19 +1,21 @@
 class Solution {
-  public boolean checkStraightLine(int[][] coordinates) {
-    int x0 = coordinates[0][0];
-    int y0 = coordinates[0][1];
-    int x1 = coordinates[1][0];
-    int y1 = coordinates[1][1];
-    int dx = x1 - x0;
-    int dy = y1 - y0;
+    public boolean checkStraightLine(int[][] coordinates) {
+        int x0=coordinates[0][0];
+        int y0=coordinates[0][1];
+        int x1=coordinates[1][0];
+        int y1=coordinates[1][1];
 
-    for (int i = 2; i < coordinates.length; ++i) {
-      int x = coordinates[i][0];
-      int y = coordinates[i][1];
-      if ((x - x0) * dy != (y - y0) * dx)
-        return false;
+        int dx=x1-x0;
+        int dy=y1-y0;
+
+        for(int i=2;i<coordinates.length;i++){
+            int x=coordinates[i][0];
+            int y=coordinates[i][1];
+
+            if((x-x0)*dy!=dx*(y-y0)){
+                return false;
+            }
+        }
+        return true;
     }
-
-    return true;
-  }
 }
