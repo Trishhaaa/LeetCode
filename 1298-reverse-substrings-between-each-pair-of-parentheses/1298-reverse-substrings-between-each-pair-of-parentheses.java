9625e17f68e8aa554441d622958a34d2d9443021
Trishhaaa <97701078+Trishhaaa@users.noreply.github.com>
2024-07-11 09:53:16 +0530

    Time: 4 ms (54.97%), Space: 42.3 MB (39.04%) - LeetHub

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String reverseParentheses(String s) {
+        Stack<Character> stack= new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch==')'){
+                StringBuilder sb= new StringBuilder();
+                while(stack.peek()!='('){
+                    sb.append(stack.pop());
+                }
+                stack.pop();
+
+                for(char reversedChar : sb.toString().toCharArray()){
+                    stack.push(reversedChar);
+                }
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+        StringBuilder result= new StringBuilder();
+        for(char ch: stack){
+            result.append(ch);
+        }
+
+        return result.toString();
+    }
+}
