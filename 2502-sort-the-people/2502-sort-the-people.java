cd1f3c1a2af8f43d7713bc9587be7fc2b509b2c5
Trishhaaa <97701078+Trishhaaa@users.noreply.github.com>
2024-07-22 19:26:52 +0530

    Time: 8 ms (62.47%), Space: 45.4 MB (32.21%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -6,31 +6,15 @@ class Solution {
             map.put(heights[i],names[i]);
         }
 
-        sortDescending(heights);
+        Arrays.sort(heights);
 
         String[] result= new String[names.length];
-
-        for(int i=0;i<heights.length;i++){
-            result[i]=map.get(heights[i]);
+        int index=0;
+        for(int i=heights.length-1;i>=0;i--){
+            result[index]=map.get(heights[i]);
+            index++;
         }
         return result;
     }
-    public static void sortDescending(int[] array) {
-        int n = array.length;
-        boolean swapped;
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (array[j] < array[j + 1]) {
-                    // Swap array[j] and array[j + 1]
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-            // If no elements were swapped, the array is already sorted
-            if (!swapped) break;
-        }
-    }
+    
 }
