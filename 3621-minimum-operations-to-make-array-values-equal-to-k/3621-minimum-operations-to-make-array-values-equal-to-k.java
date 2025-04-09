caa7f6f35ec272d1a4485a08f424fe763f5aa338
Trishhaaa <97701078+Trishhaaa@users.noreply.github.com>
2025-04-09 21:41:55 +0530

    Time: 3 ms (52.54%), Space: 44.4 MB (73.22%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.java
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.java
@@ -1,18 +1,15 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        Arrays.sort(nums);
-        if(nums[0]<k){
-            return -1;
-        }
-        int count=0;
+        HashMap <Integer, Integer> map= new HashMap<>();
         for(int i=0;i<nums.length;i++){
+            if(nums[i]<k){
+                return -1;
+            }
+
             if(nums[i]>k){
-                count++;
-                while(i+1<nums.length && nums[i]==nums[i+1]){
-                    i++;
-                }
+                map.put(nums[i], map.getOrDefault(nums[i],0)+1);
             }
         }
-        return count;
+        return map.size();
     }
 }
