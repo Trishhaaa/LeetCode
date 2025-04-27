20077784ca0073dee5f8397bae654f608ad0c8aa
Trishhaaa <97701078+Trishhaaa@users.noreply.github.com>
2025-04-27 12:20:04 +0530

    Time: 2 ms (66.43%), Space: 41.9 MB (15.06%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -4,15 +4,19 @@ class Solution {
             return true;
         }
 
-        int i=0,j=0;
+        int i=0;
+        int j=0;
         while(i<s.length() && j<t.length()){
             if(s.charAt(i)==t.charAt(j)){
                 i++;
+                j++;
+            }
+            else{
+                j++;
             }
             if(i==s.length()){
                 return true;
             }
-            j++;
         }
         return false;
     }
